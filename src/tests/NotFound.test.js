d484106a475c742fdd36771557186e74d0d0ca4e
BernardoJFilho <bernardojcm.filho@gmail.com>
2023-04-04 20:08:32 -0300

    termina questao4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,17 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('testa page NotFound', () => {
+  test('', () => {
+    renderWithRouter(<NotFound />);
+    const textNotFound = screen.getByRole('heading', { name: /page requested not found/i, level: 2 });
+    expect(textNotFound).toBeInTheDocument();
+  });
+  test('', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img', { name: /pikachu crying because the page requested was not found/i });
+    expect(img.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
